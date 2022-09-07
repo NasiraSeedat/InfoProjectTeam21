8892351abddded9cec669a44d1d61ac284bf6f72
Ruan94bez <Ruan.94bezuidenhout@gmail.com>
2022-09-07 14:38:47 +0200

    Added Code to Disease and Person
    Added information for UML diagram to the Classes.
    Made SurvivalRate an overloaded function

--- InfoTeamProject21/InfoTeamProject21/Disease.vb
+++ InfoTeamProject21/InfoTeamProject21/Disease.vb
@@ -1,3 +1,43 @@
-﻿Public Class Disease
+﻿Option Strict On
+Option Explicit On
+Option Infer Off
+Public Class Disease
+    '<<Attributes>>
+    Private _NumberInfected As Integer
+    Private _DeathRate As Integer
+    Private _Name As String
 
+    '<<Property>>
+    Public Property NumberInfected() As Integer
+        Get
+            Return _NumberInfected
+        End Get
+        Set(value As Integer)
+            _NumberInfected = value
+        End Set
+    End Property
+
+    Public Property DeathRate As Integer
+        Get
+            Return _DeathRate
+        End Get
+        Set(value As Integer)
+            _DeathRate = value
+        End Set
+    End Property
+
+    Public Property Name As String
+        Get
+            Return _Name
+        End Get
+        Set(value As String)
+            _Name = value
+        End Set
+    End Property
+
+    '<<Methoods>>
+    'calclulate the TransmitionRates of the diseases
+    Public Overridable Function TransmitionRates(DiseasesPrevalence As Double) As Double
+        Return NumberInfected / DiseasesPrevalence
+    End Function
 End Class
