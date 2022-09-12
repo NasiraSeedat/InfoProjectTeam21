98395592e9eb7e0d6d6daa33afeda8b09395b78a
clet78 <cletusmoyo1978@gmail.com>
2022-09-12 22:15:09 +0200

    Update Form1.vb
    Update initial form code

--- InfoTeamProject21/InfoTeamProject21/Form1.vb
+++ InfoTeamProject21/InfoTeamProject21/Form1.vb
@@ -1,3 +1,24 @@
-﻿Public Class Form1
+﻿' *****************************************************************
+' Team Number: assigned to team
+' Team Member 1 Details: Moyo, CT (221039267)
+' Team Member 2 Details: Seedat, S ()
+' Team Member 3 Details: Mabaire, N ()
+' Team Member 4 Details: e.g. Ruzuidenhout, R ()
+' Practical: Team Project
+' Class name: (frmDiseases)
+' *****************************************************************
+
+'My option statements...
+Option Strict On
+Option Explicit On
+Option Infer Off
+
+
+Public Class frmDiseases
+    Private Malaria As Malaria
+    Private HIV As HIVandAIDS
+    Private nP As Integer
+
 
 End Class
+
