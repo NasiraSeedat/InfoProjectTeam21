751e6fc63a438da05e96f4fde401d498581cc3b1
clet78 <cletusmoyo1978@gmail.com>
2022-09-14 19:41:00 +0200

    Added seedoc calculation and transmission rate.

--- InfoTeamProject21/InfoTeamProject21/HIVandAIDS.vb
+++ InfoTeamProject21/InfoTeamProject21/HIVandAIDS.vb
@@ -1,3 +1,101 @@
+<<<<<<< Updated upstream
 ﻿Public Class HIVandAIDS
+=======
+﻿Option Strict On
+Option Explicit On
+Option Infer Off
+Public Class HIVandAIDS
+    Inherits Disease
+    'ATTRIBUTES
+    Private _YearsWith As Double
+    Private _TypefARV As String
+    Private _SeeDoc As Boolean
+
+    'PROPERTY METHODS
+    Public Property YearsWith() As Double
+        Get
+            Return _YearsWith
+        End Get
+        Set(value As Double)
+            _YearsWith = value
+        End Set
+    End Property
+
+    Public Property TypefARV() As String
+        Get
+            Return _TypefARV
+        End Get
+        Set(value As String)
+            _TypefARV = value
+        End Set
+    End Property
+
+    'CONSTRUCTORS
+    'Parameter-less
+    Public Sub New()
+        MyBase.New()
+    End Sub
+    'Parameterised
+    'Public Sub New(YearsWith As Double)
+    '    _YearsWith = YearsWith
+    'End Sub
+
+    'CLASS METHODS
+    'Function to determine the type of ARV the person will have to use 
+    Public Function CalcTypefARV(YearsWith As Double) As String
+        Select Case (YearsWith)
+            Case <= 2
+                _TypefARV = "Single Tablet Regimens"
+            Case 2 To 8
+                _TypefARV = "Protease Inhibitors"
+            Case > 8
+                _TypefARV = "Nucleoside reverse transcriptase inhibitors"
+        End Select
+
+        Return _TypefARV
+    End Function
+
+    'Function to determine whether the person should see a doctor or not 
+    Public Overloads Function SeeDoctor() As Boolean
+        If (_YearsWith < 5) Then
+            _SeeDoc = False
+        Else
+            _SeeDoc = True
+        End If
+
+        Return (_SeeDoc)
+    End Function
+
+    Public Function SurvivalRate(HAART As Boolean, TimeInfected As Integer) As Double 'Calculate survival rate if person has HIV/AIDS
+        'HAART is a treatment for HIV/AIDS
+        'TimeInfected is the time that the patient has had the disease
+
+        If HAART = True Then
+            Select Case TimeInfected
+                Case <= 2
+                    Return 87
+                Case 2 To 4
+                    Return 86
+                Case 4 To 8
+                    Return 78
+                Case > 8
+                    Return 61
+            End Select
+        Else
+            Select Case TimeInfected
+                Case <= 2
+                    Return 48
+                Case 2 To 4
+                    Return 26
+                Case > 4
+                    Return 18
+            End Select
+        End If
+    End Function
+
+    Public Overrides Function TransmitionRates(DiseasesPrevalence As Double) As Double
+        Return NumberInfected / DiseasesPrevalence
+    End Function
+>>>>>>> Stashed changes
 
 End Class
--- InfoTeamProject21/InfoTeamProject21/Malaria.vb
+++ InfoTeamProject21/InfoTeamProject21/Malaria.vb
@@ -1,3 +1,81 @@
+<<<<<<< Updated upstream
 ﻿Public Class Malaria
+=======
+﻿Option Strict On
+Option Explicit On
+Option Infer Off
+Public Class Malaria
+    Inherits Disease
+    'ATTRIBUTES
+    Private _ParasiteType As Integer
+    Private _NumSymptoms As Integer 'set as integer to make it easier to determine whether on should see a doctor
+    Private _SeeDoc As Boolean
+
+    'PROPERTY METHODS
+    Public Property ParasiteType() As Integer
+        Get
+            Return _ParasiteType
+        End Get
+        Set(value As Integer)
+            _ParasiteType = value
+        End Set
+    End Property
+
+    Public Property NumSymptoms() As Integer
+        Get
+            Return _NumSymptoms
+        End Get
+        Set(value As Integer)
+            _NumSymptoms = value
+        End Set
+    End Property
+
+    'CONSTRUCTORS
+
+    Public Sub New(n As Integer, d As Double, name As String)
+        MyBase.New()
+    End Sub
+
+    Public Sub New(NumSymptoms As Integer)
+        NumSymptoms = _NumSymptoms
+    End Sub
+
+    'CLASS METHODS
+    'Function to determine whether the person should see a doctor
+    'If the person has a specific type of parasite and a certain number of symptoms they will either need to see a doctor or not see a doctor 
+    Public Overloads Function SeeDoctor(ByVal _ParasiteType As Integer, ByVal _NumSymptoms As Integer, ByRef _SeeDoc As Boolean) As Boolean
+        Select Case (_ParasiteType)
+            Case 1
+                If _NumSymptoms < 5 Then
+                    _SeeDoc = False
+                Else
+                    _SeeDoc = True
+                End If
+            Case 2
+                If _NumSymptoms < 6 Then
+                    _SeeDoc = False
+                Else
+                    _SeeDoc = True
+                End If
+            Case 3
+                If _NumSymptoms < 4 Then
+                    _SeeDoc = False
+                Else
+                    _SeeDoc = True
+                End If
+            Case 4
+                If _NumSymptoms < 2 Then
+                    _SeeDoc = False
+                Else
+                    _SeeDoc = True
+                End If
+        End Select
+        Return _SeeDoc
+    End Function
+>>>>>>> Stashed changes
+
+    Public Overrides Function TransmitionRates(DiseasesPrevalence As Double) As Double
+        Return NumberInfected / DiseasesPrevalence
+    End Function
 
 End Class
